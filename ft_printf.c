0233087d6efbf8a2476e243dc80366c5c6988c74
mistery5677 <miguel.costa.5677@gmail.com>
2024-04-19 11:17:32 +0100

    updated

--- ft_printf.c
+++ ft_printf.c
@@ -44,25 +44,25 @@ static int	ft_conversion(va_list args, const char format)
 	int	len;
 
 	len = 0;
-
-	if (format == '\0')
-		return (0);
-	if (format == 'c')
-		len += ft_putchar(va_arg(args, int));
-	else if (format == 'd' || format == 'i')
-		len += ft_printnbr(va_arg(args, int));
-	else if (format == 'u')
-		len += ft_print_unsigned(va_arg(args, unsigned int));
-	else if (format == 'x')
-		len += ft_print_hex(va_arg(args, unsigned int), 'a');
-	else if (format == 'X')
-		len += ft_print_hex(va_arg(args, unsigned int), 'A');
-	else if (format == 'p')
-		len += ft_print_add(va_arg(args, long long));
-	else if (format == 's')
-		len += ft_printstr(va_arg(args, const char *));
-	else if (format == '%')
-		len += ft_putchar('%');
+	if(args != NULL)
+	{
+		if (format == 'c')
+			len += ft_putchar(va_arg(args, int));
+		else if (format == 'd' || format == 'i')
+			len += ft_printnbr(va_arg(args, int));
+		else if (format == 'u')
+			len += ft_print_unsigned(va_arg(args, unsigned int));
+		else if (format == 'x')
+			len += ft_print_hex(va_arg(args, unsigned int), 'a');
+		else if (format == 'X')
+			len += ft_print_hex(va_arg(args, unsigned int), 'A');
+		else if (format == 'p')
+			len += ft_print_add(va_arg(args, long long));
+		else if (format == 's')
+			len += ft_printstr(va_arg(args, const char *));
+		else if (format == '%')
+			len += ft_putchar('%');
+	}
 	return (len);
 }
 
@@ -87,8 +87,8 @@ int	ft_printf(const char *format, ...)
 		else
 			len += ft_putchar(format[i]);
 	}
-	va_end(args);
 	if (protect(format) % 2 != 0)
 		return (-1);
+	va_end(args);
 	return (len);
 }
--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 
 int main()
 {
-    ft_printf(" %d\n", ft_printf("%%%d%", 1));
-    ft_printf(" %d\n", ft_printf("%%%d%", 1));
-    printf(" %d\n", printf("%%%d%", 1));
-    printf(" %d\n", printf("%%%d%", 1));
+    ft_printf(" %d\n", ft_printf("%d"));
+    ft_printf(" %d\n", ft_printf("%d"));
+    printf(" %d\n", printf("%d%"));
+    printf(" %d\n\n", printf("%d%"));
+
+    ft_printf(" %d\n", ft_printf("%"));
+    ft_printf(" %d\n", ft_printf("%"));
+    printf(" %d\n", printf("%"));
+    printf(" %d\n\n", printf("%"));
+
+    ft_printf(" %d\n", ft_printf(NULL));
+    ft_printf(" %d\n", ft_printf("%d%c", 1, 'a'));
+    printf(" %d\n", printf(NULL));
+    printf(" %d\n", printf("%d%c", 1, 'a'));
 }
